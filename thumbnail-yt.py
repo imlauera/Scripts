1d1a956557c863dc60fbcad505bf7560eb52fc8a
nisteeklod <suurrend3r@gmail.com>
2020-04-30 04:02:57 -0300

    updated not_fond function and sys.argv

--- thumbnail-yt.py
+++ thumbnail-yt.py
@@ -4,27 +4,29 @@ import webbrowser
 import sys
 import re
 
-# didn't find a better way to do it,lol
-THUMBNAIL_NOT_FOUND = requests.get('https://i.ytimg.com/vi/not_found/maxdefault.jpg').text
 
-def not_found(thumbnail):
+def not_found(thumbnail,THUMBNAIL_NOT_FOUND):
     # If they are equal then that thumbnail doesn't exist.
     return requests.get(thumbnail).text == THUMBNAIL_NOT_FOUND
 
 
 def get_thumbnail(url):
+    # didn't find a better way to do it,lol
+    THUMBNAIL_NOT_FOUND = requests.get('https://i.ytimg.com/vi/not_found/maxdefault.jpg').text
+
     r = requests.get(url)
     ID = re.search('watch\?v=(.*)',url).group(1)
 
     thumbnails = "https://i.ytimg.com/vi/%s/maxresdefault.jpg;https://i.ytimg.com/vi/%s/hqdefault.jpg" % (ID,ID)
 
     for thumbnail in thumbnails.split(';'):
-      if not not_found(thumbnail):
+      if not not_found(thumbnail,THUMBNAIL_NOT_FOUND):
         print(thumbnail)
         webbrowser.open(thumbnail, new=2)
 
 if __name__ == '__main__':
-  if len(sys.argv) < 2:
-    print('%s <yt url>') % sys.argv[0]
+  if len(sys.argv) == 1:
+    print('./%s <yt url>' % sys.argv[0])
+    exit()
 
   get_thumbnail(sys.argv[1])
