575ff197fb402b087794d1e29474421c19736fdb
nisteeklod <suurrend3r@gmail.com>
2020-04-30 04:41:20 -0300

    efficiency progress

--- thumbnail-yt.py
+++ thumbnail-yt.py
@@ -14,14 +14,18 @@ def get_thumbnail(url):
     # didn't find a better way to do it,lol
     THUMBNAIL_NOT_FOUND = requests.get('https://i.ytimg.com/vi/not_found/maxdefault.jpg').text
 
-    ID = re.search('watch\?v=(.*)',url).group(1)
+    VIDEO_ID = re.search('watch\?v=(.*)',url).group(1)
 
-    thumbnails = "https://i.ytimg.com/vi/%s/maxresdefault.jpg;https://i.ytimg.com/vi/%s/hqdefault.jpg" % (ID,ID)
+    # This thumbnail always exists, so just show it up.
+    hq_thumbnail = "https://i.ytimg.com/vi/%s/hqdefault.jpg" % (VIDEO_ID)
+    print(hq_thumbnail)
+    webbrowser.open(hq_thumbnail, new=2)
 
-    for thumbnail in thumbnails.split(';'):
-      if not not_found(thumbnail,THUMBNAIL_NOT_FOUND):
-        print(thumbnail)
-        webbrowser.open(thumbnail, new=2)
+    max_thumbnail = "https://i.ytimg.com/vi/%s/maxresdefault.jpg" % (VIDEO_ID)
+    # the max thumbnail not always exists, so I gotta check it.
+    if not not_found(max_thumbnail,THUMBNAIL_NOT_FOUND):
+      print(max_thumbnail)
+      webbrowser.open(max_thumbnail, new=2)
 
 if __name__ == '__main__':
   if len(sys.argv) == 1:
