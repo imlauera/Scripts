8dfe94d30965c9ea42012dc75146d2d552e1ef5c
nisteeklod <suurrend3r@gmail.com>
2020-05-19 06:52:10 -0300

    Now it save the wallpaper on your /tmp

--- get_wallpaper.sh
+++ get_wallpaper.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/bash
-if [ ! "$(command -v feh)" ]; then echo "bro, you need to install feh"; exit; fi
-
 # RATING: questionable, explicit, safe
 RATING="questionable"
+
+if [ ! "$(command -v feh)" ]; then echo "bro, you need to install feh"; exit; fi
+
 RESOLUTION=$(xrandr | grep '*' | awk '{print $1}') &&
 WIDTH=$(echo $RESOLUTION | cut -d 'x' -f 1) &&
 HEIGHT=$(echo $RESOLUTION | cut -d 'x' -f 2) &&
 echo "Your resolution is $WIDTH x $HEIGHT" &&
-GET_WALL=$(curl -s https://konachan.com/post.json\?tags\=%20width:$WIDTH%20height:$HEIGHT%20order:wide%20rating:$RATING%20order:random limit:1 | 
-	 jq -r '.[0]["file_url"]') &&
+GET_WALL=$(curl -s https://konachan.com/post.json?tags=%20width:$WIDTH%20height:$HEIGHT%20rating:$RATING%20order:random limit:1 | jq -r '.[0]["file_url"]') &&
 echo "Downloading and setting wallpaper" &&
-feh --bg-max $GET_WALL &&
+ext=${GET_WALL##*.} &&
+FILE="/tmp/current.$ext" &&
+curl -s $GET_WALL --output - > $FILE &&
+echo "Saved in $FILE" &&
+feh --bg-max $FILE &&
 echo "Done"
